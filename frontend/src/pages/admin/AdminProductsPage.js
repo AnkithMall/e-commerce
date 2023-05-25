728ce4f31b22cb5dadc3b6f75c7726a332959b6f
ankith <20mcme19@uohyd.ac.in>
2023-05-25 21:04:47 +0530

    admin pages is added

--- frontend/src/pages/admin/AdminProductsPage.js
+++ frontend/src/pages/admin/AdminProductsPage.js
@@ -1,5 +1,66 @@
+import { Button, Col, Row, Table } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import AdminLinksComponents from "../../components/admin/AdminLinksComponents";
+
 const AdminProductsPage = () => {
-    return <p>This is admin products page</p>
+    return (
+        <Row className="m-5">
+            <Col md={2}>
+                <AdminLinksComponents />
+            </Col>
+            <Col md={10}>
+                <h1>Product List {" "}
+                    <LinkContainer to="/admin/create-new-product">
+                        <Button variant="primary" size="lg">
+                            Create New
+                        </Button>
+                    </LinkContainer>
+
+                </h1>
+                <Table striped bordered hover responsive>
+                    <thead>
+
+                        <tr>
+                            <th>#</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th>Edit/Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {[{ name: "Panasonic", price: "$250", category: "TV" },
+                        { name: "HP", price: "$1000", category: "Laptops" },
+                        { name: "Avengers", price: "$350", category: "Games" }].map((item, idx) => (
+                            <tr key={idx}>
+                                <td>{idx + 1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.price}</td>
+                                <td>{item.category}</td>
+                                <td>
+                                    <LinkContainer to="/admin/edit-product">
+                                        <Button className="btn-sm">
+                                            <i className="bi bi-pencil-square"></i>
+                                        </Button>
+                                    </LinkContainer>
+                                    /
+                                    <Button className="btm-sm" variant="danger">
+
+                                    </Button>
+
+                                </td>
+                                <td>PayPal</td>
+                                <td>
+
+                                </td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
+    );
 }
 
-export default AdminProductsPage ;
+export default AdminProductsPage;
--- frontend/src/pages/user/UserCartDetailsPage.js
+++ frontend/src/pages/user/UserCartDetailsPage.js
@@ -1,5 +1,86 @@
+import { Alert, Button, Col, Container, Form, ListGroup, Row } from "react-bootstrap";
+import CartItemComponent from "../../components/CartItemComponent";
+
 const UserCartDetailsPage = () => {
-    return <p>This is user cart details page</p>
+    return (
+        <Container fluid>
+            <Row className="mt-4">
+                <h1>Cart Details</h1>
+                <Col md={8}>
+                    <br />
+                    <Row>
+                        <Col md={6}>
+                            <h2>Shipping</h2>
+                            <b>Name</b>: Ankith <br />
+                            <b>Address</b>: 19/27 Balajinagar, secunderabad 500087 <br />
+                            <b>Phone</b>: 6301570910
+                        </Col>
+                        <Col md={6}>
+                            <h2>Payment Method</h2>
+                            <Form.Select >
+                                <option value="pp">
+                                    PayPal
+                                </option>
+                                <option value="cod">
+                                    Cash On Delivery
+                                </option>
+                            </Form.Select>
+                        </Col>
+                        <Row>
+                            <Col>
+                                <Alert className="mt-3" variant="danger">
+                                    Not Delivered
+                                </Alert>
+                            </Col>
+                            <Col>
+                                <Alert className="mt-3" variant="success">
+                                    Not Paid Yet
+                                </Alert>
+                            </Col>
+                        </Row>
+                    </Row>
+                    <br />
+                    <h2>Order Items</h2>
+                    <ListGroup variant="flush">
+                        {Array.from({ length: 3 }).map((item, idx) => (
+                            <CartItemComponent key={idx} />
+                        ))}
+                    </ListGroup>
+                </Col>
+                <Col md={4}>
+                    <ListGroup>
+                        <ListGroup.Item>
+                            <h3>Order Summary</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Item Price (after gst) : <span className="fw-bold">$892</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Shipping : <span className="fw-bold">included</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Tax : <span className="fw-bold">included</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="text-danger">
+                            Total Price : <span className="fw-bold">$904</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item >
+                            <div className="d-grid gap-2">
+                                <Button size="lg" variant="danger" type="button">
+                                    Pay for the Order
+                                </Button>
+                            </div>
+
+                        </ListGroup.Item>
+
+                    </ListGroup>
+
+                </Col>
+
+            </Row>
+
+        </Container>
+    );
 }
 
-export default UserCartDetailsPage ;
+export default UserCartDetailsPage;
